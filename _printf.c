15543209693eee5bd35c9376504b619f0680d709
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-06-29 21:56:53 -0500

    what i think the printf folder format should look like. also i wrote what i think is the base function for the printf function located in the _printf.c file

---
+++ _printf.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+int _print(const char *format, ...)
+{
+    va_list ap;
+    int char_len = 0;
+
+    va_start (ap, format);
+
+    if (format == NULL)
+        return (-1);
+
+    while (*format != '\0')
+    {
+       if (*format == '%')
+       {
+           format++;
+           len += output_cases(format, ap);
+       }
+       else
+           len += write(STDOUT_FILENO, &(*format), :);
+       format++;
+   }
+   va_end(ap);
+   return (char_len);
+
+}   
